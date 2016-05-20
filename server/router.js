5bee8497c8448daea514edab279bfa7a54892247
jcursoli <joeycursoli2@gmail.com>
2016-05-20 14:04:43 -0700

    [setup] creates database config

--- server/router.js
+++ server/router.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const Bookshelf = require('bookshelf')(knex);
 
 module.exports = (app) => {
-	app.get('/', function(req,res, next){
+	app.get('/', function(req, res, next){
 		res.sendFile(path.join(__dirname, '../client/index.html'));
 	});
+	app.post('/signin',function(req, res){})
 	
 }
