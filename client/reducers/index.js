bb604c60c71f89744610bc56d7cec22652a4b481
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-19 17:28:31 -0700

    [setup] initializes actions, reducers, and index

---
+++ client/reducers/index.js
@@ -0,0 +1,8 @@
+// Combine reducers will go here
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+	state: (state = {}) => state
+});
+
+export default rootReducer;
