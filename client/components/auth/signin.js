df9c82f4fc80f165ec23dcb50ed837bd14c47270
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-19 19:13:18 -0700

    [pull] implements basic signin form with redux-form and redux-thunk

--- client/components/auth/signin.js
+++ client/components/auth/signin.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form'; 
+import * as actions from '../../actions';
 
-export default class Signin extends Component {
+class Signin extends Component {
+	handleFormSubmit({ email, password }) {
+		this.props.signinUser({ email, password });
+	}
 
 	render() {
+		const { handleSubmit, fields: { email, password }} = this.props;
+
 		return (
-			<form>
-				<fieldset>
+			<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+				<fieldset className="form-group">
 					<label>Email:</label>
-					<input />
+					<input {...email} className="form-control"/>
+					{email.touched && email.error && <div className="error">{email.error}</div>}
 				</fieldset>
-				<fieldset>
+				<fieldset className="form-group">
 					<label>Password:</label>
-					<input />
+					<input {...password} type="password" className="form-control"/>
+					{password.touched && password.error && <div className="error">{password.error}</div>}
 				</fieldset>
 				<button action="submit" className="btn btn-primary">Sign in</button>
 			</form>
 		);
 	}
 }
+
+function mapStateToProps(state){
+	return { errorMessage: state.auth.error };
+}
+
+function validate(formProps) {
+	const errors = {};
+
+	if (!formProps.email) {
+		errors.email = 'Please enter an email';
+	}
+
+	if (!formProps.password) {
+		errors.password = 'Please enter a password';
+	}
+
+	return errors;
+}
+
+export default reduxForm({
+	form: 'signin',
+	fields: ['email','password'],
+	validate
+}, mapStateToProps, actions)(Signin);
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,12 @@
 // Combine reducers will go here
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import authReducer from './authReducer';
 
 
 const rootReducer = combineReducers({
-	form
+	form,
+	auth: authReducer
 });
 
 export default rootReducer;
