c8a74bdb761d6777ef9a05fc1bcb6170646a3054
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-20 22:10:03 -0700

    [pull] adds filler data and placeholder profile pic to profile page

--- client/components/profile.js
+++ client/components/profile.js
@@ -6,24 +6,44 @@ export default class Profile extends Component {
 
 	render() {
 		return (
-			<div className="container">
+			<div className="container-fluid">
 				<div className="row">
-					<div class="col-sm-4">
-			      <div className="media">
-			      	<img className="media-object" src="https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png
-" />
-			      </div>
-			    </div>
-			    <div class="col-sm-4">
-			      <div>
-			      	Info
-			      </div>
-			    </div>
-			    <div class="col-sm-4">
-			      <div>
-			      	Matches
-			      </div>
-			    </div>
+					<div className="row-height">
+						<div className="col-md-4">
+							<div>
+								<h3 className="text-xs-center">Profile</h3>
+							</div>
+				      <div>
+				      	<div className="text-xs-center">
+				      		<img className="img-rounded center-block" src="https://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png" />
+				      		<h4>Firstname Lastname</h4>
+				      		<h5>Email@email.com</h5>
+				      	</div>
+				      </div>
+				    </div>
+				    <div className="col-md-4">
+				      <div>
+				      	<h3 className="text-xs-center">Info</h3>
+				      	<div>
+				      		<h5>Language:</h5>
+				      		<div>
+				      			JavaScript
+				      		</div>
+				      	</div>
+				      	<div>
+				      		<h5>Skill Level:</h5>
+				      		<div>
+				      			Beginner
+				      		</div>
+				      	</div>
+				      </div>
+				    </div>
+				    <div className="col-md-4">
+				      <div>
+				      	<h3 className="text-xs-center">Matches</h3>
+				      </div>
+				    </div>
+					</div>
 				</div>
 			</div>
 		);
