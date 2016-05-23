61625eaa5108dfd6ceccd308b224e7c4f7fb71e4
Andrew Patipaksiri <apax714@gmail.com>
2016-05-23 15:41:07 -0700

    [setup] adds material-ui to dependencies and index

--- client/index.js
+++ client/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import App from './components/app';
 import Landing from './components/landing';
@@ -17,6 +19,7 @@ const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
+   <MuiThemeProvider muiTheme={getMuiTheme()}>
     <Router history={browserHistory}>
     	<Route path="/" component={App}>
         <IndexRoute component={Landing} />
@@ -25,5 +28,6 @@ ReactDOM.render(
         <Route path="profile" component={Profile} />
     	</Route>
     </Router>
+	 </MuiThemeProvider>
   </Provider>
   , document.getElementById('app'));
