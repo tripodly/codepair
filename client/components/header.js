1abc5b4829264053a789c3114f01f7463f3eb249
Andrew Patipaksiri <apax714@gmail.com>
2016-05-23 16:10:54 -0700

    [style] adds appbar to header component and styles buttons and title

--- client/components/header.js
+++ client/components/header.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+
+const style = {
+	link: {
+		textDecoration: 'none',
+		color: 'white',
+	},
+	titleLink: {
+		textDecoration: 'none',
+		color: 'white',
+		float: 'left',
+	},
+	bar: {
+		verticalAlign: 'middle',
+	},
+	links: {
+		paddingTop: 5,
+	},
+}
 
 export default class Header extends Component {
 
 	render() {
 		return (
-			<nav className="navbar navbar-light">
-				<span className="navbar-brand">CodePair</span>
-				<ul className="nav navbar-nav">
-					<li className="nav-item">
-						<Link to={"/signin"}>Sign In</Link>
-					</li>
-					<li className="nav-item">
-						<Link to={"/signup"}>Sign Up</Link>
-					</li>
-					<li className="nav-item">
-						<Link to={"/"}>Sign Out</Link>
-					</li>
-				</ul>
-			</nav>
+			<AppBar style={style.bar}
+				zDepth={1}
+				showMenuIconButton={false}
+				title={<Link style={style.titleLink} to={"/"}><span>CodePair</span></Link>}
+				iconElementRight={
+						<div style={style.links}>
+							<Link style={style.link} to={"/signin"}><FlatButton style={style.link} label="Sign In" /></Link>
+							<Link style={style.link} to={"/signup"}><FlatButton style={style.link} label="Sign Up" /></Link>
+							<Link style={style.link} to={"/"}><FlatButton style={style.link} label="Sign Out" /></Link>
+						</div>
+					}
+				/>
 		);
 	}
 }
