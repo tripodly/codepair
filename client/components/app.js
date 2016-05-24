96bedac54388cee89b00167d5596183e9a21a9d1
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-23 19:40:53 -0700

    [style] fixes style alignment in header

--- client/components/app.js
+++ client/components/app.js
@@ -4,15 +4,12 @@ import Paper from 'material-ui/Paper';
 
 const style = {
 		paper: {
-				width: '100%',
 				textAlign: 'center',
-				display: 'inline-block',
 				minHeight: 900,
 		},
 		app: {
 				paddingRight: 40,
 				paddingLeft: 40,
-				margin: 'auto',
 				minHeight: 1000,
 		},
 
