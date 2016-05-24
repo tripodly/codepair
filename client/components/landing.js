8d02eb4ab82128debc2ba07f9e6e3b5fad4f7d30
Andrew Patipaksiri <apax714@gmail.com>
2016-05-23 20:04:18 -0700

    [pull] centers elements on page

--- client/components/landing.js
+++ client/components/landing.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
+import Paper from 'material-ui/Paper';
+import { Link } from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  child: {
+    marginTop: 20,
+  },
+  innerChild: {
+    width: '100%',
+  },
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}
 
 export default class Landing extends Component  {
   render() {
 	 return (
-		<div>
-			Welcome to CodePair!
-      <div id="video_stage">
-        <ReactPlayer url ='https://www.youtube.com/watch?v=dQw4w9WgXcQ' playing={true} />
+    <div style={style}>
+      <div style={style.child}>
+        <h2>Welcome to CodePair!</h2>
+        <ReactPlayer url ='https://www.youtube.com/watch?v=dQw4w9WgXcQ' playing={false} />
+        <RaisedButton label={"Sign Up"} fullWidth={true} secondary={true}/>
       </div>
-		</div>
+    </div>
 	 );
   }
 }
