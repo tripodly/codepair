bec7d2fe604b1977cbdf09b58fba3333a3202f26
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-26 12:24:18 -0700

    [pull] adds example query for creating a match

--- server/db/exampleQueries.sql
+++ server/db/exampleQueries.sql
@@ -8,3 +8,8 @@ SELECT * FROM `passes` WHERE `toUser` = 25 OR `fromUser` = 25;
 
 # Selects all relationships in matches where current user is the fromUser(initiator) or toUser(recipient):
 SELECT * FROM `matches` WHERE `toUser` = 25 OR `fromUser` = 25;
+
+# Deletes relationship from pending and turns it into a relationship in matches,
+# in this case the user_id of the user shown in the current card is 14
+DELETE FROM `pendings` WHERE `pendings`.`fromUser` = 14 AND `pendings`.`toUser` = 25;
+INSERT INTO `matches` (`fromUser`, `toUser`) VALUES (25, 14);
