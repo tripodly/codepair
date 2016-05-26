9bcadbc89a5e919167aaa16a3fbcbd7b6a0a062a
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-26 12:46:45 -0700

    [pull] adds example queries for selecting matches and pending matches

--- server/db/exampleQueries.sql
+++ server/db/exampleQueries.sql
@@ -13,3 +13,21 @@ SELECT * FROM `matches` WHERE `toUser` = 25 OR `fromUser` = 25;
 # in this case the user_id of the user shown in the current card is 14
 DELETE FROM `pendings` WHERE `pendings`.`fromUser` = 14 AND `pendings`.`toUser` = 25;
 INSERT INTO `matches` (`fromUser`, `toUser`) VALUES (25, 14);
+
+
+# TO GET MATCHES (placed into matches array):
+## Selects all user records who've matched with currentUser of id 14:
+SELECT U.* FROM `users` AS U WHERE U.id IN (
+	SELECT M.`fromUser` FROM `matches` AS M WHERE `toUser` = 14 
+    UNION
+	SELECT M.`toUser` FROM `matches` AS M WHERE `fromUser` = 14);
+
+# TO GET PENDINGS (placed into initiated array):
+## Selects all user records who've swiped right on currentUser of id 20:
+SELECT U.* FROM `users` AS U WHERE U.id IN (
+	SELECT P.`fromUser` FROM `pendings` AS P WHERE `toUser` = 20);
+
+# TO GET OTHERS (placed into uninitiated array):
+## Selects records of all users who have not liked nor disliked the current user and who the current user has not liked or disliked:
+SELECT U.* FROM `users` AS U WHERE U.id IN (
+	);
