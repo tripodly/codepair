0851725809d737736d028dfb35d2ef694ea53d2f
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-26 15:42:07 -0700

    [feature] completes footer with link to org repo

--- client/components/footer.js
+++ client/components/footer.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import Header from './header';
+import IconButton from 'material-ui/IconButton';
+import Love from 'material-ui/svg-icons/action/favorite';
+import FontIcon from 'material-ui/FontIcon';
 
 const style = {
   footer: {
@@ -11,6 +14,12 @@ const style = {
     backgroundColor: '#212121',
     color: '#585858',
   },
+  footerBar: {
+
+  },
+  icon: {
+    color: '#585858',
+  }
 
 }
 export default class Footer extends Component {
@@ -19,7 +28,8 @@ export default class Footer extends Component {
     return (
       <footer style={style.footer} >
         <div style={style.footerBar}>
-          <h5>blahblahblah</h5>
+          <div>Made with</div><div><Love style={style.icon} color="#585858"/></div><div> by Colin Zarnegar, Joey Cursoli, and Andrew Patipaksiri</div>
+          <div><IconButton linkButton={true} href="https://github.com/tripodly/codepair"><FontIcon style={style.icon} className="muidocs-icon-custom-github" color="#585858" hoverColor="#FFF"/></IconButton></div>
         </div>
       </footer>
     );
