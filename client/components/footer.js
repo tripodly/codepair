03f28b80f5b1d986e9dafc771e06527008c495f3
Andrew Patipaksiri <apax714@gmail.com>
2016-05-26 18:13:05 -0700

    [fix] fixes merge conflict in footer

--- client/components/footer.js
+++ client/components/footer.js
@@ -27,8 +27,11 @@ export default class Footer extends Component {
   render(){
     return (
       <footer style={style.footer} >
+        <div style={style.footerBar}>
+          <div>Made with</div><div><Love style={style.icon} color="#585858"/></div><div> by Colin Zarnegar, Joey Cursoli, and Andrew Patipaksiri</div>
+          <div><IconButton linkButton={true} href="https://github.com/tripodly/codepair"><FontIcon style={style.icon} className="muidocs-icon-custom-github" color="#585858" hoverColor="#FFF"/></IconButton></div>
+        </div>
       </footer>
     );
   }
 }
-
