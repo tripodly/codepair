95e353fefcf9b20a332c01ef3572d0fd11187191
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-26 17:18:47 -0700

    [pull] makes progress on ace editor component

--- client/components/codeshare.js
+++ client/components/codeshare.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AceEditor from 'react-ace';
 
+import 'brace/mode/java';
+import 'brace/theme/github';
+
+
+
 class CodeShare extends Component {
 
+	onChange(newValue) {
+		console.log('values have changed, they are now : ',newValue);
+	}
 
 	render() {
 		return (
-			<div>Testing 1...2...3...</div>
+			<div>
+				<div>Testing 1...2...3...</div>
+				<AceEditor
+				    mode="java"
+				    theme="github"
+				    onChange={(value) => this.onChange(value)}
+				    name="UNIQUE_ID_OF_DIV"
+				    editorProps={{$blockScrolling: true}}
+				  />
+			</div>
 		);
 	}
 };
