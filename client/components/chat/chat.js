16fd3cbbd1f183e7b9973cb9f7b7a0d90b6a6fda
Andrew Patipaksiri <apax714@gmail.com>
2016-05-27 16:29:24 -0700

    [fix] moved chat.js into chat subfolder

---
+++ client/components/chat/chat.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import Draggable, { DraggableCore } from 'react-draggable';
+
+import Message from './chat/message';
+import MessageList from './chat/messageList';
+
+
+export default class Chat extends Component {
+ 
+  render() {
+    return (
+      <Draggable>
+          <div className="wrapper">
+            <nav id="nav" className="nav">
+              <div className="default-nav">
+                <div className="main-nav">
+                  <div className="toggle"></div>
+                  <div className="main-nav-item"><a href="#" className="main-nav-item-link">Andrew</a></div>
+                  <div className="options"></div>
+                </div>
+              </div>
+            </nav>
+            <div id="inner" className="inner">
+              <div id="content" className="content"></div>
+            </div>
+            <div id="bottom" className="bottom">
+              <textarea id="input" className="input"></textarea>
+              <div id="send" className="send"></div>
+            </div>
+          </div>
+      </Draggable>
+    )
+  }
+}
