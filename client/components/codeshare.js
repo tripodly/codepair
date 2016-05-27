d43f09bbdac06e98495e5a878bb4aa79603906a0
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-26 17:16:25 -0700

    [setup] adds react-ace to dependencies

---
+++ client/components/codeshare.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import AceEditor from 'react-ace';
+
+class CodeShare extends Component {
+
+
+	render() {
+		return (
+			<div>Testing 1...2...3...</div>
+		);
+	}
+};
+
+export default CodeShare;
