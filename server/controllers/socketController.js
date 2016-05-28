bcc55eea595923143f3d5a488a7bf374c0ede263
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-27 18:53:45 -0700

    [pull] builds out socketController

--- server/controllers/socketController.js
+++ server/controllers/socketController.js
@@ -4,10 +4,12 @@ var io = require('../server').io;
 // console.log('in socketController : ',io);
 var sockets = [];
 var people = {};
+var rooms = [];
 
 io.on('connection', function(socket) {
 	console.log('Client has connected to server!');
 	sockets.push(socket);
+	// console.log('socket connection id is : ',socket.conn.id);
 
 	socket.on('join', function(user){
 		if(user.id) {
@@ -18,6 +20,9 @@ io.on('connection', function(socket) {
 
 	socket.on('partner', function(partnerObject){
 		console.log('partnerObject is : ',partnerObject);
+		var roomName = ''+partnerObject.fromUser.id+":"+partnerObject.toUser.id+'';
+		console.log('new room created, id is : ',roomName);
+		rooms.push(roomName);
 	})
 
 	//disconnect from the server
