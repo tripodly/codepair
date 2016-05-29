7a3fdf47b236f20c01957e9226a07d6f2f51f86d
jcursoli <joeycursoli2@gmail.com>
2016-05-29 15:36:24 -0700

    [pull] builds out forum component

--- client/components/forum.js
+++ client/components/forum.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
 import * as actions from '../actions';
 import DropDownMenu from 'material-ui/DropDownMenu';
+import TextField from 'material-ui/TextField';
 import MenuItem from 'material-ui/MenuItem';
 
 const style = {
-	codeWindow: {
+	forumWindow: {
 		textAlign: 'left',
 		width: '100%',
 		height: 800,
@@ -22,23 +24,53 @@ const style = {
 	optionsMenu: {
 		marginTop: -15,
 	},
+	textField:{
+		border:' 3px solid #585858',
+		marginTop: 15,
+		marginBottom:15,
+		width: '50%',
+		height: 200,
+	},
+	subject:{
+		width: '50%',
+		border:' 3px solid #585858',
+	},
+	newPost:{
+		height: 400,
+		marginTop: 25,
+		marginLeft: 150,
+		marginRight: 150,
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		backgroundColor:'#D8D8D8',
+	},
 }
+//when user wants to create a new post, make this true
+let flag = false;
 
 class Forum extends Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			filter: 'mostRecent',
-			psots: [],
-			comments: []
+			filter: 'Most Recent',
+			posts: [],
+			comments: [],
+			input: ''
 		}
 	}
-
+	handleClick(){
+		console.log('handlde click was clicked in the forum conpment');
+	}
 	handleChange(event, index, value) {
 		this.setState({
 			filter: value
 		})
+	}
+	handlChangeInput(event){
+		this.setState({input:event.target.value})
+		console.log(event.target.value);
 	}
 
 	componentDidMount(){
@@ -46,16 +78,34 @@ class Forum extends Component {
 	}
 	render() {
 		return (
-			<div style={style.codeWindow}>
+			<div style={style.forumWindow}>
 				<AppBar style={style.optionsBar} showMenuIconButton={false} iconElementRight={
-					<DropDownMenu style={style.optionsMenu} value={this.state.language} onChange={(event, index, value) => this.handleChange(event, index, value)}>
-	          <MenuItem value={'Up-Votes'} primaryText="JavaScript" />
-	          <MenuItem value={'Most Recent'} primaryText="Java" />
-	          <MenuItem value={'Most Comments'} primaryText="Python" />
+					<DropDownMenu style={style.optionsMenu} value={this.state.filter} onChange={(event, index, value) => this.handleChange(event, index, value)}>
+	          <MenuItem value={'Most Recent'} primaryText="Most Recent" />
+	          <MenuItem value={'Up-Votes'} primaryText="Up-Votes" />
+	          <MenuItem value={'Most Comments'} primaryText="Most Comments" />
 	        </DropDownMenu>
 				}/>	
-				<div>
-					{'this is a test'}
+				<div style={style.newPost}>
+					<br></br>
+					<RaisedButton label="New Post" primary={true} style={style} onClick={()=>this.handleClick()} />
+					<br></br>
+					<TextField 
+						style={style.subject}
+						placeholder={'Subject'}
+						multiLine={false}
+						name="POST_SUBJECT"
+					/>
+					<TextField 
+						style={style.textField}
+						placeholder={'Body...'}
+						multiLine={true}
+						rows={10}
+					  rowsMax={15}
+						name="POST_COMPONENT"
+						value={this.state.input}
+						onChange={(e)=> this.handlChangeInput(e)}
+					/>
 				</div>		
 			</div>
 		);
@@ -65,6 +115,5 @@ class Forum extends Component {
 function mapStateToProps(state) {
 	return { posts: state.posts, comments: state.comments };
 }
-
 
 export default connect(mapStateToProps, actions)(Forum);
