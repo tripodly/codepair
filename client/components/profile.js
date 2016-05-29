247245f044beee794110b754b41c21617d3669c5
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-29 12:14:48 -0700

    [setup] adds react-bootstrap to dependencies

--- client/components/profile.js
+++ client/components/profile.js
@@ -111,68 +111,53 @@ class Profile extends Component {
 				 <CircularProgress size={2} />
 				</div>
 			)
-		} else{
-		return (
-			<div className="container-fluid">
-				<div className="row">
-					<div className="row-height">
-						<div className="col-md-4">
-							<div>
-								<h3 className="text-xs-center">Profile</h3>
-							</div>
-				      <div>
-				      	<div className="text-xs-center">
-				      		<img style={style.profilePic} className="img-rounded center-block" src={this.props.profilePicture} />
-				      		<h4>{this.props.profileName}</h4>
-				      		<h5>{this.props.profileEmail}</h5>
-				      	</div>
-				      </div>
-				    </div>
-				    <div className="col-md-4">
-				      <div>
-				      	<div style={style.editButton} onClick={() => this.handleEditInfo()} className="btn btn-primary">{prompt}</div>
-				      	<div>
-				      		<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-				      			<fieldset className="form-group">
-				      				<label>Email:</label>
-				      				<input {...email} onChange={(event)=> this.handleOnChangeInput(event,'email')}  value={this.state.email} disabled={flag} className="form-control" placeholder={this.props.profileEmail}/>
-				      				{email.touched && email.value && <div className="error">{email.error}</div>}
-				      			</fieldset>
-				      			<fieldset className="form-group">
-				      				<label>Name:</label>
-				      				<input {...name} value={this.state.name} onChange={(event)=> this.handleOnChangeInput(event,'name')} className="form-control" disabled={flag} placeholder={this.props.profileName}/>
-				      				{name.touched && name.error && <div className="error">{name.error}</div>}
-				      			</fieldset>
-				      			<fieldset className="form-group">
-				      				<label>Language:</label>
-				      				<input {...language} onChange={(event)=> this.handleOnChangeInput(event,'language')} className="form-control"  value={this.state.language} disabled={flag} placeholder={this.props.profileLanguage}/>
-				      				{language.touched && language.error && <div className="error">{language.error}</div>}
-				      			</fieldset>
-				      			<fieldset className="form-group">
-				      				<label>Skill level:</label>
-				      				<input {...skillLevel} onChange={(event)=> this.handleOnChangeInput(event,'skill')} className="form-control"  value={this.state.skill} disabled={flag} placeholder={this.props.profileSkillLevel}/>
-				      				{skillLevel.touched && skillLevel.error && <div className="error">{skillLevel.error}</div>}
-				      			</fieldset>
-				      			<button action="submit" className="btn btn-primary" hidden={flag} onClick={()=> this.handleEditInfo()}>Confirm</button>
-				      		</form>
-				      	</div>
-				      </div>
-				    </div>
-				    <div className="col-md-4">
-				      <div>
-				      	<h3 className="text-xs-center">Matches</h3>
-				      	<List>
-				      		{ this.props.matches.map(match =>
-				      			<MatchItem context={this} handleClick={this.handleListItemClick} match={match} /> 
-				      		)}
-				      	</List>
-				      	<Link to="/cards"><button>Match me!</button></Link>
-				      </div>
-				    </div>
+		} else {
+			return (
+				<div>
+					<div>
+						<img style={style.profilePic} src={this.props.profilePicture} />
+						<h4>{this.props.profileName}</h4>
+						<h5>{this.props.profileEmail}</h5>
+					</div>
+					<div>
+						<div style={style.editButton} onClick={() => this.handleEditInfo()} className="btn btn-primary">{prompt}</div>
+						<div>
+							<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+								<fieldset className="form-group">
+									<label>Email:</label>
+									<input {...email} onChange={(event)=> this.handleOnChangeInput(event,'email')}  value={this.state.email} disabled={flag} className="form-control" placeholder={this.props.profileEmail}/>
+									{email.touched && email.value && <div className="error">{email.error}</div>}
+								</fieldset>
+								<fieldset className="form-group">
+									<label>Name:</label>
+									<input {...name} value={this.state.name} onChange={(event)=> this.handleOnChangeInput(event,'name')} className="form-control" disabled={flag} placeholder={this.props.profileName}/>
+									{name.touched && name.error && <div className="error">{name.error}</div>}
+								</fieldset>
+								<fieldset className="form-group">
+									<label>Language:</label>
+									<input {...language} onChange={(event)=> this.handleOnChangeInput(event,'language')} className="form-control"  value={this.state.language} disabled={flag} placeholder={this.props.profileLanguage}/>
+									{language.touched && language.error && <div className="error">{language.error}</div>}
+								</fieldset>
+								<fieldset className="form-group">
+									<label>Skill level:</label>
+									<input {...skillLevel} onChange={(event)=> this.handleOnChangeInput(event,'skill')} className="form-control"  value={this.state.skill} disabled={flag} placeholder={this.props.profileSkillLevel}/>
+									{skillLevel.touched && skillLevel.error && <div className="error">{skillLevel.error}</div>}
+								</fieldset>
+								<button action="submit" className="btn btn-primary" hidden={flag} onClick={()=> this.handleEditInfo()}>Confirm</button>
+							</form>
+						</div>
+					</div>
+					<div>
+						<h3 className="text-xs-center">Matches</h3>
+						<List>
+							{ this.props.matches.map(match =>
+								<MatchItem context={this} handleClick={this.handleListItemClick} match={match} /> 
+							)}
+						</List>
+						<Link to="/cards"><button>Match me!</button></Link>
 					</div>
 				</div>
-			</div>
-		);
+			);
 		}
 	}
 }
@@ -223,3 +208,66 @@ export default reduxForm({
 	fields: ['email', 'name', 'language', 'skillLevel'],
 	validate
 }, mapStateToProps, actions)(Profile);
+
+
+
+
+// <div className="container-fluid">
+// 	<div className="row">
+// 		<div className="row-height">
+// 			<div className="col-md-4">
+// 				<div>
+// 					<h3 className="text-xs-center">Profile</h3>
+// 				</div>
+// 	      <div>
+// 	      	<div className="text-xs-center">
+// 	      		<img style={style.profilePic} src={this.props.profilePicture} />
+// 	      		<h4>{this.props.profileName}</h4>
+// 	      		<h5>{this.props.profileEmail}</h5>
+// 	      	</div>
+// 	      </div>
+// 	    </div>
+// 	    <div className="col-md-4">
+// 	      <div>
+// 	      	<div style={style.editButton} onClick={() => this.handleEditInfo()} className="btn btn-primary">{prompt}</div>
+// 	      	<div>
+// 	      		<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+// 	      			<fieldset className="form-group">
+// 	      				<label>Email:</label>
+// 	      				<input {...email} onChange={(event)=> this.handleOnChangeInput(event,'email')}  value={this.state.email} disabled={flag} className="form-control" placeholder={this.props.profileEmail}/>
+// 	      				{email.touched && email.value && <div className="error">{email.error}</div>}
+// 	      			</fieldset>
+// 	      			<fieldset className="form-group">
+// 	      				<label>Name:</label>
+// 	      				<input {...name} value={this.state.name} onChange={(event)=> this.handleOnChangeInput(event,'name')} className="form-control" disabled={flag} placeholder={this.props.profileName}/>
+// 	      				{name.touched && name.error && <div className="error">{name.error}</div>}
+// 	      			</fieldset>
+// 	      			<fieldset className="form-group">
+// 	      				<label>Language:</label>
+// 	      				<input {...language} onChange={(event)=> this.handleOnChangeInput(event,'language')} className="form-control"  value={this.state.language} disabled={flag} placeholder={this.props.profileLanguage}/>
+// 	      				{language.touched && language.error && <div className="error">{language.error}</div>}
+// 	      			</fieldset>
+// 	      			<fieldset className="form-group">
+// 	      				<label>Skill level:</label>
+// 	      				<input {...skillLevel} onChange={(event)=> this.handleOnChangeInput(event,'skill')} className="form-control"  value={this.state.skill} disabled={flag} placeholder={this.props.profileSkillLevel}/>
+// 	      				{skillLevel.touched && skillLevel.error && <div className="error">{skillLevel.error}</div>}
+// 	      			</fieldset>
+// 	      			<button action="submit" className="btn btn-primary" hidden={flag} onClick={()=> this.handleEditInfo()}>Confirm</button>
+// 	      		</form>
+// 	      	</div>
+// 	      </div>
+// 	    </div>
+// 	    <div className="col-md-4">
+// 	      <div>
+// 	      	<h3 className="text-xs-center">Matches</h3>
+// 	      	<List>
+// 	      		{ this.props.matches.map(match =>
+// 	      			<MatchItem context={this} handleClick={this.handleListItemClick} match={match} /> 
+// 	      		)}
+// 	      	</List>
+// 	      	<Link to="/cards"><button>Match me!</button></Link>
+// 	      </div>
+// 	    </div>
+// 		</div>
+// 	</div>
+// </div>
