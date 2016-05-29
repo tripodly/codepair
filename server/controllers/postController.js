7af90342341634e593cbe90c159c4f517c5c5521
jcursoli <joeycursoli2@gmail.com>
2016-05-29 13:07:37 -0700

    [pull] makes progress in postController to save posts to db

---
+++ server/controllers/postController.js
@@ -0,0 +1,34 @@
+var User = require('../models/user');
+var Users = require('../collections/users');
+var Post = require('../models/post');
+var Posts = require('../collections/posts');
+var Reply = require('../models/reply');
+var Replys = require('../collections/replys');
+var _ = require('lodash');
+var Promise = require('bluebird');
+var knex = require('../db/config').knex;
+
+module.exports = {
+	postMessage: function(req, res, next){
+		var messageObject = {
+			userId: req.message.id,
+			postMessage : req.message.message
+		};
+		//need to make sure message doesnt exceed limit
+		if(req.message.message && req.message.id) {
+			new Post({ 'userId': req.message.id, 'message': req.message.message }).save().then(function(postModel){
+				// postModel is the post being saved
+				res.send('post sent!');
+			})
+		} else{
+			res.send('error making post');
+		}
+	},
+	getPosts: function(req,res,next){
+
+	},
+	getComments: function(req,res,next){
+		var postID = req.post.postID;
+		//query db for all replys with postID === to req postID
+	}
+};
