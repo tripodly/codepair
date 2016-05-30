513494cd83dd4dfdbf05ba8bc0529dad03c05983
Colin Zarnegar <c.zarnegar@gmail.com>
2016-05-30 12:29:03 -0700

    [setup] adds cors module to server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var cors = require('cors');
 var app = express();
 var router = require('./router');
 var path = require('path');
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 app.use(express.static(path.join(__dirname, '../client')));
